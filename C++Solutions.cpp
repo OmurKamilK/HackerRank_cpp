2c23cc2ee53e7b9dda4d53a165cd36887b33cdad
OmurKamilK <omurkygsz@hotmail.com>
2024-04-08 16:47:31 +0300

    origin

---
+++ C++Solutions.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//1-)3 numb sum
+/*int main() {
+	int a,b,c;
+	cin>>a>>b>>c;
+	cout<<(a + b + c);
+    return 0;
+}*/
+//2-)
+/*int main() {
+	int s;
+long ld;
+char ch;
+float f;
+double d;
+cin >> s >> ld >> ch >> f >> d;
+printf("%d\n%ld\n%c\n%f\n%lf\n", s, ld, ch, f, d);
+
+    return 0;
+}*/
